218625e1d4bc4aa70346ed49aaedc61f88f642ad
csesumonpro <csesumonpro@gmail.com>
2023-09-02 15:26:47 +0600

    Added reactivity fundamentals

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <h2>Hello from App</h2>
+  <p>{{ name }}</p> -  {{ user.email }}
+  <br/>
+  <input type="text" v-model="name">
+  <input type="email" v-model="user.email">
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script setup>
+import { reactive, ref } from 'vue';
+
+const name = ref('John Doe');
+
+const user = reactive({
+  name: 'John Doe',
+  email: 'jon@gmail.com'
+});
+
 </script>
 
 <style>
